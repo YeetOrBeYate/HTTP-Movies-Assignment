9c8bec07aa17a5474625856d0278e5debb69d442
Kyle Corbin <kylecorbin26@gmail.com>
2019-11-14 20:10:53 -0500

    figured out how to edit the stars array

--- client/src/Movies/MovieUpdate.js
+++ client/src/Movies/MovieUpdate.js
@@ -30,9 +30,24 @@ const MovieUpdate = (props)=>{
         setUpdate({...update, [e.target.name]: e.target.value})
     }
 
+    const handleStar = (e)=>{
+
+        const newStars = update.stars.slice();
+
+        newStars[e.target.name] = e.target.value;
+        console.log(newStars)
+
+        setUpdate({...update, ...update.stars = newStars})
+
+     
+     
+     
+    }
+
     const handleSub = (e)=>{
         e.preventDefault();
         console.log("update",update);
+        // axios.put(`http://localhost:5000/api/movie/${update.id}`)
         setUpdate({
             ...update,
             title:"",
@@ -57,10 +72,10 @@ const MovieUpdate = (props)=>{
                 <label>Metascore:</label>
                 <input name="metascore" value={update.metascore} onChange={handleInput}/>
             </div>
-            {update.stars.map((star)=>(
+            {update.stars.map((star, index)=>(
                 <div>
                 <label>Stars:</label>
-                <input name="stars" value={star} onChange={handleInput}/>
+                <input name={index} value={star} onChange={handleStar}/>
             </div>
             ))}
             <button onClick={handleSub}>Edit</button>
